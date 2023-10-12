eae2a21b8f1af35aee9f7b57b7410b06d59c8e84
Miko <michaelcapote.mc@gmail.com>
2023-10-12 09:57:05 +0800

    -batch update

--- RUNS/miko.bat
+++ RUNS/miko.bat
@@ -1,2 +1,2 @@
 cd C:\Users\mcapote\PycharmProjects\WebAppsV11_RF
-robot -d C:\Users\mcapote\PycharmProjects\WebAppsV11_RF\Reports --timestampoutputs .\RSS_LPL_RF\TestCases\Scripts\TCCSF165_SearchCatalog\TCCSF165_SearchCatalog.robot
+robot -d C:\Users\mcapote\PycharmProjects\WebAppsV11_RF\Reports --timestampoutputs .\RSS_LPL_RF\TestCases\Scripts\Punchout\TCCSF170_PunchoutUI\TCCSF170_Punchout_UI.robot
